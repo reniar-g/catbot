54bdba9bf29e8dce738440d696692d1e956f26ae
angele34 <90606365+angele34@users.noreply.github.com>
2025-11-26 10:13:41 +0800

    feat: define training hyperparameters for catbot

---
+++ catbot/training.py
@@ -0,0 +1,79 @@
+import random
+import time
+from typing import Dict
+import numpy as np
+import pygame
+from utility import play_q_table
+from cat_env import make_env
+#############################################################################
+# TODO: YOU MAY ADD ADDITIONAL IMPORTS OR FUNCTIONS HERE.                   #
+#############################################################################
+
+#############################################################################
+# END OF YOUR CODE. DO NOT MODIFY ANYTHING BEYOND THIS LINE.                #
+#############################################################################
+
+def train_bot(cat_name, render: int = -1):
+    env = make_env(cat_type=cat_name)
+    
+    # Initialize Q-table with all possible states (0-9999)
+    # Initially, all action values are zero.
+    q_table: Dict[int, np.ndarray] = {
+        state: np.zeros(env.action_space.n) for state in range(10000)
+    }
+
+    # Training hyperparameters
+    episodes = 5000 # Training is capped at 5000 episodes for this project
+    
+    #############################################################################
+    # TODO: YOU MAY DECLARE OTHER VARIABLES AND PERFORM INITIALIZATIONS HERE.   #
+    #############################################################################
+    # Hint: You may want to declare variables for the hyperparameters of the    #
+    # training process such as learning rate, exploration rate, etc.            #
+    #############################################################################
+    alpha = 0.1 # How fast to learn (higher = faster but less stable)
+    gamma = 0.9 # Discount factor
+    epsilon = 1.0 # Start with 100% random actions
+    epsilon_min = 0.05 
+    epsilon_decay = 0.999 # Reduce exploration over time
+    
+    #############################################################################
+    # END OF YOUR CODE. DO NOT MODIFY ANYTHING BEYOND THIS LINE.                #
+    #############################################################################
+    
+    for ep in range(1, episodes + 1):
+        ##############################################################################
+        # TODO: IMPLEMENT THE Q-LEARNING TRAINING LOOP HERE.                         #
+        ##############################################################################
+        # Hint: These are the general steps you must implement for each episode.     #
+        # 1. Reset the environment to start a new episode.                           #
+        # 2. Decide whether to explore or exploit.                                   #
+        # 3. Take the action and observe the next state.                             #
+        # 4. Since this environment doesn't give rewards, compute reward manually    #
+        # 5. Update the Q-table accordingly based on agent's rewards.                #
+        ############################################################################## 
+        # 1. Reset environment to start a new episode
+        state, info = env.reset()
+        done = False
+
+        # Until bot has not caught cat, keep training it
+        while not done: 
+            # 2. Explore or exploit
+            if random.random() < epsilon:
+                action = env.action_space.sample() # explore
+            else:
+                action = np.argmax(q_table[state]) # exploit
+
+           
+        
+        #############################################################################
+        # END OF YOUR CODE. DO NOT MODIFY ANYTHING BEYOND THIS LINE.                #
+        #############################################################################
+
+        # If rendering is enabled, play an episode every 'render' episodes
+        if render != -1 and (ep == 1 or ep % render == 0):
+            viz_env = make_env(cat_type=cat_name)
+            play_q_table(viz_env, q_table, max_steps=100, move_delay=0.02, window_title=f"{cat_name}: Training Episode {ep}/{episodes}")
+            print('episode', ep)
+
+    return q_table
